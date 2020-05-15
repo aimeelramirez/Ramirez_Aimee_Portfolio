2f1dbc06255cc9cf941213111949afc5d58a56c6
Aimee Lynn Ramirez <aimeelynnramirez@Aimees-MacBook-Pro.local>
2020-05-15 00:07:49 -0400

    validations for inputs

--- SDI/ArrayList/ArrayList.js
+++ SDI/ArrayList/ArrayList.js
@@ -4,8 +4,8 @@ window.onload = function() {
 // let inputWantingToBuy;
 // let numberWantingToBuy;
 let input, parseInput;
-let isParsed; 
-let answer;
+let isParsed, isBookParsed; 
+let answer, answerBook;
 let total = [];
  //now to add the number of the array
  let stringArray = [];
@@ -40,64 +40,106 @@ const validateInt = (parseInput, isParsed) =>{
     return isParsed;
 
 }
+const validateDecimal = (inputBookPrice) =>{
+    //console.log(isParsed +"check 1" + parseInput)
+     // set the NaN to have a value
+     isBookParsed = 0.0;
+      //let check = !isNaN(parseFloat(inputBookPrice));
+      //let checkIn = isFinite(inputBookPrice);
+      console.log("Sorry, this can only be a number.")
+      alert("Sorry, this can only be a number.")
+   
+       while(inputBookPrice != isBookParsed){
+      //  console.log(inputBookPrice +" before ** check book decimal" + isBookParsed)
+
+       inputBookPrice = prompt(`Please, enter the price again, like 2 for $2 or $2.00:`);
+       parseInput = validateString(inputBookPrice);
+       isBookParsed = parseFloat(parseInput);
+       //console.log(inputBookPrice +" after ** check book decimal" + isBookParsed)
+
+       }
+      // console.log(isParsed +"check 3" + parseInput)
+       return isBookParsed;
+   
+   }
+
+const addBooks = (answer) =>{
+
+ console.log(`Great you wanted these number of books: ${answer}`);
+
+    for(let a = 0; a < answer; a++){
+
+       // console.log(answer+ "check!!" + isParsed);
+      let inputBookPrice = prompt(`Please, enter the ${a+1} price as a like 2 for $2 or $2.00:`);
+     //check if empty
+      parseInput = validateString(inputBookPrice);
+      let checkNaNBook = Number.isNaN(parseFloat(parseInput));
+      
+       //console.log(parseInput + "->" + checkNaNBook);
+      if(checkNaNBook == true){
+
+        //console.log(a + "# wrong data types caught");
+
+          //check if it is a decimal
+         isBookParsed = validateDecimal(parseInput);
+          //after passing prompt push it into the array
+          stringArray.push(isBookParsed);
+          console.log(stringArray + " array string false");
+          total.push(isBookParsed);
+      
+      } 
+      else if(checkNaNBook != true) {
+      total.push(parseFloat(parseInput));
+    }
+    
+    //console.log(total+ " total array to add up");
+    
+    //reduce to sum it up
+     sum = total.reduce((a, b) =>{
+        return a + b;
+    });
+    
+    console.log(parseFloat(sum).toFixed(2)+ "sum total");
+    console.log(`price of book #${a+1} $${total[a].toFixed(2)}`);
+     console.log(`sum of all books $${sum.toFixed(2)}`);
+     document.getElementById("listPrice").innerHTML +=  `${a+1}`+") $"+ total[a].toFixed(2) +"<br/>";
+
+
+      }
+      document.getElementById("sum").innerHTML += "<hr/><h3>Total:</h3><h2> $"+ sum.toFixed(2) +"</h2>";
+
+
+
+    }
+
 
 input = prompt("how many books would you like to purchase? (Please input a whole number like 1, 3, or 5)")
 parseInput = validateString(input);
 isParsed = parseInt(parseInput);
 
 if(parseInput == isParsed ){
-    
+    //console.log(isParsed + ":" + parseInput)
+   //if equal than no reason to int check
     console.log(`Great you wanted these number of books: ${parseInput}`);
     alert(`Great you wanted these number of books: ${parseInput}`);
     document.getElementById("name").innerHTML += "<h3> Great! you wanted these number of books: "+ parseInput +"</h3>";
-   
-    for(let a = 0; a < isParsed; a++){
-        let inputBookPrice = prompt(`Please, enter the ${a+1} price as a like 2 for $2 or $2.00:`);
-        stringArray.push(+inputBookPrice);
-       //console.log(stringArray[a]+ "\n");
-       total.push(+parseFloat(stringArray[a]).toFixed(2));
-       sum += total[a];
-       let index = a+1;
-       console.log(`price of book #${a+1} $${parseFloat(stringArray[a]).toFixed(2)}`);
-       document.getElementById("listPrice").innerHTML += index +") $"+ total[a].toFixed(2) +"<br/>";
-    }
-    console.log(`sum of all books $${sum.toFixed(2)}`);
-
-    document.getElementById("sum").innerHTML += "<hr/><h3>Total:</h3><h2> $"+ sum.toFixed(2) +"</h2>";
+    answer = isParsed;
+    answerBook = addBooks(answer);
 
 
  }else if(isParsed != parseInput){
      //check the NaN
+    // console.log(isParsed + ":" + parseInput)
+
     let checkNaN = Number.isNaN(isParsed);
     // check undefined
-   // console.log(isParsed + ":" + parseInput)
     if(isParsed == undefined || checkNaN == true){
 
-            console.log('ERROR');
+           // console.log('ERROR!');
             answer = validateInt(parseInput, isParsed);
             document.getElementById("name").innerHTML += "<h3> Fantastic! you wanted these number of books: "+  answer +"</h3>";
-            console.log(`Great you wanted these number of books: ${answer}`);
- 
-  for(let a = 0; a < answer; a++){
-     let inputBookPrice = prompt(`Please, enter the ${a+1} price as a like 2 for $2 or $2.00:`);
-  
-        stringArray.push(+inputBookPrice);
-        console.log(stringArray[a]);
-        //validate int
-       //total += parseInt(stringArray[a]);
-       total.push(+parseFloat(stringArray[a]).toFixed(2));
-       sum += total[a];
-       console.log(`price of book #${a+1} $${parseFloat(stringArray[a]).toFixed(2)}`);
-
-       console.log(`sum of all books $${sum.toFixed(2)}`);
-       document.getElementById("listPrice").innerHTML += index +") $"+ total[a].toFixed(2) +"<br/>";
-        document.getElementById("sum").innerHTML += "<hr/><h3>Total:</h3><h2> $"+ sum.toFixed(2) +"</h2>";
-
-      }
-    }else{
-        console.log('ERROR');
+            answerBook = addBooks(answer);
     }
-
 
  }
  var stringFruits = ["grapes", "apples", "limes", "lemon", "ball", "carrot", "towel", "laptop", "stove"];
@@ -111,4 +153,4 @@ if(parseInput == isParsed ){
  }
 
    
-} 
+}
